b982f45c1f5b8be58119471ad4eeb3fbe611b7d1
Vitaliy Nesterov <liyuop@mail.ru>
2019-08-16 15:31:39 +0300

    Initial Commit

---
+++ SuperConcentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperConcentration
+//
+//  Created by Виталий on 16/08/2019.
+//  Copyright © 2019 Aperantim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
